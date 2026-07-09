f6e08b39da588a345ebb7a6094a0634000f2007e
douglasc2001 <douglasc_2001@hotmail.com>
2026-07-09 16:50:40 -0600

    corrections made to the branch

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,43 @@
+import { qs, getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
+        qs("#addToCart").addEventListener(
+            "click",
+            this.addToCart.bind(this)
+        );
+    }
+
+    addToCart() {
+        let cartItems = getLocalStorage("so-cart");
+        if (!Array.isArray(cartItems)) {
+            cartItems = cartItems ? [cartItems] : [];
+        }
+        cartItems.push(this.product);
+        setLocalStorage("so-cart", cartItems);
+    }
+
+    renderProductDetails() {
+        qs("#productName").textContent = this.product.NameWithoutBrand;
+        qs("#productBrand").textContent = this.product.Brand.Name;
+
+        const productImage = qs("#productImage");
+        productImage.src = this.product.Image;
+        productImage.alt = this.product.NameWithoutBrand;
+
+        qs("#productPrice").textContent = this.product.FinalPrice;
+        qs("#productColor").textContent = this.product.Colors[0].ColorName;
+        qs("#productDescription").innerHTML = this.product.DescriptionHtmlSimple;
+
+        qs("#addToCart").dataset.id = this.product.Id;
+    }
+}
+
