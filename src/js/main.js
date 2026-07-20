5051ff92f1d517a932bff3e7d7a2919021e99c0e
lionel-alvarado16 <lalvaradoanastacio@byupathway.edu>
2026-07-20 18:12:57 -0500

    W02 Individual Activity Completed

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const dataSource = new ProductData("tents");
+const productList = new ProductList("tents");
+
+console.log(productList, dataSource);
