ca341f29af8b1d7364789fd2717946b9426d64ca
lionel-alvarado16 <lalvaradoanastacio@byupathway.edu>
2026-07-22 01:20:08 -0500

    W03 Team Activity Completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
-const productList = new ProductList("tents");
+const element = document.querySelector(".product-list");
+const productList = new ProductList("tents", dataSource, element);
 
-console.log(productList, dataSource);
+productList.init();
--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -9,6 +9,8 @@ const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+
+loadHeaderFooter();
 
 // // add to cart button event handler
 // async function addToCartHandler(e) {
