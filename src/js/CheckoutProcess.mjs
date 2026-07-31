b1f500a7adef40b74087d4395206ec55c273a680
lionel-alvarado16 <lalvaradoanastacio@byupathway.edu>
2026-07-31 01:53:15 -0500

    W04 Individual Activity Completed

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, alertMessage, removeAllAlerts } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
 
 const services = new ExternalServices();
@@ -99,7 +99,15 @@ export default class CheckoutProcess {
         try {
             const response = await services.checkout(order);
             console.log(response);
+            setLocalStorage("so-cart", []);
+            location.assign("/checkout/success.html");
         } catch (err) {
+            // get rid of any preexisting alerts.
+            removeAllAlerts();
+            for (let message in err.message) {
+                alertMessage(err.message[message]);
+            }
+            
             console.log(err);
         }
     }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -16,3 +16,11 @@ document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
   order.checkout();
 });
+
+// this is how it would look if we listen for the submit on the form
+// document.forms["checkout"]
+// .addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   // e.target would contain our form in this case
+//   order.checkout();
+// });
